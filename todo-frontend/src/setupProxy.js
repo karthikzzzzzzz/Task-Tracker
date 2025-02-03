0da0bc23cb3e463ef021e3a78649989e6ec70254
Karthik Rajan S <132420926+karthikzzzzzzz@users.noreply.github.com>
2025-02-04 00:41:06 +0530

    Update setupProxy.js

--- todo-frontend/src/setupProxy.js
+++ todo-frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api', 
     createProxyMiddleware({
-      target: 'http://backend.eba-9ypvtqbe.us-east-1.elasticbeanstalk.com',
+      target: 'http://backend:8000',
       changeOrigin: true,
       pathRewrite: {'^/api' : ''} 
     })
